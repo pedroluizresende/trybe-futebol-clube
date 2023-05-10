50f7b835387bc1c45a5f13d71bde9574f603c0d2
Pedro Resende <pedroluizrnunez@gmail.com>
2023-05-10 18:24:21 -0300

    requisito 1 completo

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,27 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  declare id:number;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
+
+export default Team;
